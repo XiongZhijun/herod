dd161eee1149efa57f1d34989fcfc954330f097c
XiongZhijun <hust.xzj@gmail.com>
2013-06-07 20:55:26 +0800

    抽取出一个抽象的“商品列表”Fragment类，便于后面用于显示搜索出的商品时使用。

--- herod-phone-buyer/src/org/herod/buyer/phone/fragments/GoodsListFragment.java
+++ herod-phone-buyer/src/org/herod/buyer/phone/fragments/GoodsListFragment.java
@@ -6,68 +6,26 @@ package org.herod.buyer.phone.fragments;
 import java.util.Collections;
 import java.util.List;
 
-import org.herod.buyer.phone.BaseActivity;
 import org.herod.buyer.phone.BuyerContext;
-import org.herod.buyer.phone.HerodTask;
-import org.herod.buyer.phone.HerodTask.AsyncTaskable;
 import org.herod.buyer.phone.R;
-import org.herod.buyer.phone.ShoppingCartCache;
 import org.herod.framework.MapWrapper;
 import org.herod.framework.adapter.SimpleAdapter;
-import org.herod.framework.adapter.SimpleAdapter.ViewBinder;
-import org.herod.framework.widget.XListView;
-import org.herod.framework.widget.XListView.IXListViewListener;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * @author Xiong Zhijun
  * @email hust.xzj@gmail.com
  * 
  */
-public class GoodsListFragment extends Fragment implements
-		AsyncTaskable<Long, List<MapWrapper<String, Object>>>,
-		IXListViewListener, ViewBinder {
-	private XListView goodsListView;
+public class GoodsListFragment extends AbstractGoodsListFragment {
 	private MapWrapper<String, Object> goodsType;
 	private long latestGoodsId = 0;
-	private SimpleAdapter adapter;
-	private long shopId;
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_goods_list, container,
-				false);
-		shopId = getArguments().getLong("shopId");
-		goodsListView = (XListView) view.findViewById(R.id.goodsListView);
-		goodsListView.setPullRefreshEnable(false);
-		goodsListView.setPullLoadEnable(true);
-		// goodsListView.setOnItemClickListener(this);
-		goodsListView.setXListViewListener(this);
-		adapter = new SimpleAdapter(getActivity(),
-				Collections.<MapWrapper<String, Object>> emptyList(),
-				R.layout.fragment_goods_list_item, new String[] { "name",
-						"price", "name", "name", "name" }, new int[] {
-						R.id.name, R.id.price, R.id.quantity, R.id.addButton,
-						R.id.reduceButton });
-		adapter.setViewBinder(this);
-		goodsListView.setAdapter(adapter);
-		return view;
-	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
 		latestGoodsId = 0;
-		new HerodTask<Long, List<MapWrapper<String, Object>>>(this).execute();
 	}
 
 	@Override
@@ -79,81 +37,27 @@ public class GoodsListFragment extends Fragment implements
 
 	@Override
 	public void onPostExecute(List<MapWrapper<String, Object>> data) {
-		adapter.addData(data);
+		super.onPostExecute(data);
 		latestGoodsId = data.get(data.size() - 1).getLong("id");
-		goodsListView.stopLoadMore();
 	}
 
 	@Override
-	public boolean setViewValue(final View dataSetView,
-			final MapWrapper<String, Object> dataSet, final View view,
-			String from, int to, int position, Object data,
-			String textRepresentation) {
-		final long goodsId = dataSet.getLong("id");
-		if (to == R.id.quantity) {
-			int quantity = ShoppingCartCache.getInstance().getQuantity(shopId,
-					goodsId);
-			setQuantity(dataSetView, quantity);
-			return true;
-		}
-		if (to == R.id.reduceButton) {
-			view.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					decrease(dataSetView, goodsId);
-				}
-			});
-		}
-		if (to == R.id.addButton) {
-			view.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					increase(dataSetView, dataSet);
-				}
-			});
-		}
-		return false;
+	protected SimpleAdapter createAdapter(FragmentActivity activity) {
+		return new SimpleAdapter(getActivity(),
+				Collections.<MapWrapper<String, Object>> emptyList(),
+				R.layout.fragment_goods_list_item, new String[] { "name",
+						"price", "name", "name", "name" }, new int[] {
+						R.id.name, R.id.price, R.id.quantity, R.id.addButton,
+						R.id.reduceButton });
 	}
 
-	private void increase(View dataSetView, MapWrapper<String, Object> dataSet) {
-		int quantity = ShoppingCartCache.getInstance()
-				.increase(shopId, dataSet);
-		setQuantity(dataSetView, quantity);
-	}
-
-	private void decrease(View dataSetView, long goodsId) {
-		int quantity = ShoppingCartCache.getInstance()
-				.decrease(shopId, goodsId);
-		setQuantity(dataSetView, quantity);
-	}
-
-	private void setQuantity(View dataSetView, int quantity) {
-		TextView quantityView = (TextView) dataSetView
-				.findViewById(R.id.quantity);
-		View reduceButton = dataSetView.findViewById(R.id.reduceButton);
-		if (quantity > 0) {
-			quantityView.setText(quantity + "");
-			quantityView.setVisibility(View.VISIBLE);
-			reduceButton.setVisibility(View.VISIBLE);
-		} else {
-			quantityView.setVisibility(View.INVISIBLE);
-			reduceButton.setVisibility(View.INVISIBLE);
-		}
-		FragmentActivity activity = getActivity();
-		if (activity instanceof BaseActivity) {
-			((BaseActivity) activity).updateTotalQuantity();
-		}
+	@Override
+	protected long getGoodsShopId(MapWrapper<String, Object> goods) {
+		return getArguments().getLong("shopId");
 	}
 
 	public void setGoodsType(MapWrapper<String, Object> goodsType) {
 		this.goodsType = goodsType;
-	}
-
-	@Override
-	public void onRefresh() {
-	}
-
-	@Override
-	public void onLoadMore() {
-		new HerodTask<Long, List<MapWrapper<String, Object>>>(this).execute();
 	}
 
 }

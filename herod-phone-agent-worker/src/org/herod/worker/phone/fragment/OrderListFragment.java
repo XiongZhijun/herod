742a7ebccfb04204e2cbde8e1b07438e49f0fe67
XiongZhijun <hust.xzj@gmail.com>
2013-09-10 14:56:04 +0800

    删除警告

--- herod-phone-agent-worker/src/org/herod/worker/phone/fragment/OrderListFragment.java
+++ herod-phone-agent-worker/src/org/herod/worker/phone/fragment/OrderListFragment.java
@@ -28,7 +28,6 @@ import org.springframework.util.CollectionUtils;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
